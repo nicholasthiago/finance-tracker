5af6d2afd87195ae2d1054a5551ff31ed9c533c5
nkxavis2907 <nicholasxavis23@gmail.com>
2022-11-20 20:20:10 -0700

    App.routes : updated to gh-pages new path

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -14,7 +14,7 @@ const App = () => {
 			<Menu />
 
 			<Routes>
-				<Route path={'/'} element={ <Main /> } />
+				<Route path={'/finance-tracker/'} element={ <Main /> } />
 			</Routes>
 
 		</div>
